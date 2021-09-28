4bce878b3026302b3ddd52d0ac3e482741bbb48d
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-09-28 14:37:17 +0000

    I was able to complete problem 1 on my own

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 //1
 function countToFifty(){
-  
+  for(i = 1; i <= 50; i++){
+      console.log(i)
+  }
 }
-
 //2
 function countFromOne(){
   
 }
-
 //3
-function threeAndFiveInFifty(){
-  
+function threeOrFiveInFifty(){
+  for(i = 1; i <= 50; i++){
+    if(i / 3 === 0 || i / 5 === 0){
+      console.log(i)
+    }
+  }  
 }
-
 //4
 function threeAndFive(){
   
 }
 
 
+//countToFifty()
+//countFromOne()
+threeOrFiveInFifty()
